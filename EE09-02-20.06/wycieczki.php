0a39be69a656bf42109de6e977f10e31b4bcb478
Przemysław Gembalczyk <gembuspol@interia.pl>
2023-03-23 07:26:13 +0100

    egzamin
    Signed-off-by: Przemysław Gembalczyk <gembuspol@interia.pl>

---
+++ EE09-02-20.06/wycieczki.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki i urlopy</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <section class="baner">
+        <h1>BIURO PODRÓŻY</h1>
+    </section>
+    <section class="lewy">
+        <h2>KONTAKT</h2>
+        <a href="mailto:biuro@wycieczki.pl">napisz do nas</a>
+        <p>telefon: 555666777</p>
+    </section>
+    <section class="srodkowy">
+        <h2>GALERIA</h2>
+    </section>
+    <section class="prawy">
+        <h2>PROMOCJE</h2>
+        <table>
+            <tr>
+                <td>Jesień</td>
+                <td>Grupa 4+</td>
+                <td>Grupa 10+</td>
+            </tr>
+            <tr>
+                <td>5%</td>
+                <td>10%</td>
+                <td>15%</td>
+            </tr>
+        </table>
+    </section>
+    <section class="dane">
+        <h2>LISTA WYCIECZEK</h2>
+    </section>
+    <footer>
+        <p>Stronę wykonał Przemek</p>
+    </footer>
+    
+</body>
+</html>
